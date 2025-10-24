7d730a2a2378501a8bb641a83d85fccbde58125a
bingis_khan <52740618+bingis-khan@users.noreply.github.com>
2025-10-24 22:43:24 +0200

    file listing

--- libgit2.go
+++ libgit2.go
@@ -5,10 +5,14 @@ package main
 #cgo LDFLAGS: -L./libgit2/build -lgit2 -lssl -lcrypto -lz
 
 #include <git2.h>
+#include <stdio.h>
+
+extern void list_cb();
 */
 import "C"
 import "unsafe"
 import "fmt"
+
 
 func libgit_init() {
 	C.git_libgit2_init()
@@ -21,9 +25,37 @@ func repository_open(path string) (*C.git_repository, error) {
 	var repo *C.git_repository
 	error := C.git_repository_open(&repo, cpath)
 	if error < 0 {
-		e := C.git_error_last()
-		return nil, fmt.Errorf("Error %d/%d: %s\n", error, e.klass, C.GoString(e.message))
+		return nil, to_error(error)
 	}
 
 	return repo, nil
+}
+
+// quick function to list all shit in a repository
+func list_all(repo *C.git_repository) error {
+	var obj *C.git_object
+	error := C.git_revparse_single(&obj, repo, C.CString("HEAD^{tree}"))
+	if error < 0 {
+		return to_error(error)
+	}
+
+	tree := (*C.git_tree)(obj)
+
+	error = C.git_tree_walk(tree, C.GIT_TREEWALK_PRE, (*[0]byte)(C.list_cb), nil)  // note, that we're getting our Go function *through* C
+	if error < 0 {
+		return to_error(error)
+	}
+	return nil
+}
+
+//export list_cb
+func list_cb(root *C.char, entry *C.git_tree_entry, payload *C.void) {
+	name := C.GoString(C.git_tree_entry_name(entry))
+	fmt.Printf("entry: %s\n", name)
+}
+
+
+func to_error(error C.int) error {
+	e := C.git_error_last()
+	return fmt.Errorf("Error %d/%d: %s\n", error, e.klass, C.GoString(e.message))
 }
--- main.go
+++ main.go
@@ -7,11 +7,15 @@ func main() {
 	fmt.Println("asd asd cock");
 	libgit_init()
 
-	repo, err := repository_open("asdasd")
+	repo, err := repository_open(".")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)  // maybe copy the exit status of the libgit error?
 	}
 
-	_ = repo
+	err = list_all(repo)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
