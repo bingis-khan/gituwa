d0bdef6f491be5ff214c0154dd54d0c43d577b24
bingis_khan <52740618+bingis-khan@users.noreply.github.com>
2025-10-26 00:24:05 +0200

    http server and repo scanning

--- libgit2.go
+++ libgit2.go
@@ -18,7 +18,9 @@ func libgit_init() {
 	C.git_libgit2_init()
 }
 
-func repository_open(path string) (*C.git_repository, error) {
+type repo *C.git_repository
+
+func repository_open(path string) (repo, error) {
 	cpath := C.CString(path)
 	defer C.free(unsafe.Pointer(cpath))  // not sure if this resource is used after repo shit. maybe I should make a datatype which stores all the resources?
 
@@ -32,26 +34,29 @@ func repository_open(path string) (*C.git_repository, error) {
 }
 
 // quick function to list all shit in a repository
-func list_all(repo *C.git_repository) error {
+func list_all(repo *C.git_repository) ([]string, error) {
 	var obj *C.git_object
 	error := C.git_revparse_single(&obj, repo, C.CString("HEAD^{tree}"))
 	if error < 0 {
-		return to_error(error)
+		return nil, to_error(error)
 	}
 
 	tree := (*C.git_tree)(obj)
 
-	error = C.git_tree_walk(tree, C.GIT_TREEWALK_PRE, (*[0]byte)(C.list_cb), nil)  // note, that we're getting our Go function *through* C
+	var names []string
+	error = C.git_tree_walk(tree, C.GIT_TREEWALK_PRE, (*[0]byte)(C.list_cb), unsafe.Pointer(&names))  // note, that we're getting our Go function *through* C
 	if error < 0 {
-		return to_error(error)
+		return nil, to_error(error)
 	}
-	return nil
+
+	return names, nil
 }
 
 //export list_cb
 func list_cb(root *C.char, entry *C.git_tree_entry, payload *C.void) {
 	name := C.GoString(C.git_tree_entry_name(entry))
-	fmt.Printf("entry: %s\n", name)
+	names := (*[]string)(unsafe.Pointer(payload))
+	*names = append(*names, name)
 }
 
 
--- main.go
+++ main.go
@@ -2,20 +2,148 @@ package main
 
 import "fmt"
 import "os"
+import "net/http"
+import "html/template"
+import "path/filepath"
+
+// new iter stuff
+import "maps"  // maps.Keys()
+import "slices" // slices.Collect()
+// why in two different packages?
+
+var index = template.Must(template.New("page").Parse(`
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>{{.Title}}</title>
+	</head>
+	<body>
+		<h1>{{.Title}}</h1>
+		<ul>
+			{{range .Repos}}
+				<li><a href="{{.}}">{{.}}</a></li>
+			{{end}}
+		</ul>
+	</body>
+</html>
+	`))
+
+var repoPage = template.Must(template.New("repopage").Parse(`
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>{{.Title}}</title>
+	</head>
+	<body>
+		<h1>{{.Title}}</h1>
+		<ul>
+			{{range .Files}}
+				<li>{{.}}</li>
+			{{end}}
+		</ul>
+		<a href="/">back</a>
+	</body>
+</html>
+	`))
+
+func ScanForGitRepos(path string) (map[string]repo, error) {
+	libgit_init()
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	repos := make(map[string]repo)
+	for _, e := range entries {
+		if !e.IsDir() {
+			continue
+		}
+
+		repo, err := repository_open(filepath.Join(path, e.Name()))
+		if err != nil {
+			continue
+		}
+
+		repos[e.Name()] = repo
+	}
+
+	return repos, nil
+}
 
 func main() {
-	fmt.Println("asd asd cock");
-	libgit_init()
-
-	repo, err := repository_open(".")
+	repos, err := ScanForGitRepos("..")
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)  // maybe copy the exit status of the libgit error?
-	}
-
-	err = list_all(repo)
-	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Could not load repos")
 		os.Exit(1)
 	}
+
+	// index repo chooser
+	repoNames := slices.Collect(maps.Keys(repos))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		data := struct {
+			Title string
+			Repos []string
+		}{
+			Title: "repos",
+			Repos: repoNames,
+		}
+
+		if err := index.Execute(w, data); err != nil {
+			http.Error(w, "Server error", http.StatusInternalServerError)
+		}
+	})
+
+	// Interesting. Prebaking all the paths, yo!
+	//  Although I could just use r.PathValue("id") with "/{id}" path.
+	for name, repo := range repos {
+		http.HandleFunc(filepath.Join("/", name), func(w http.ResponseWriter, r *http.Request) {
+			filenames, err := list_all(repo)
+
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			if err != nil {
+				fmt.Println(err)
+				http.Error(w, "Server error", http.StatusInternalServerError)
+			}
+
+			
+			data := struct {
+				Title string
+				Files []string
+			}{
+				Title: name,
+				Files: filenames,
+			}
+
+			if err := repoPage.Execute(w, data); err != nil {
+				fmt.Println(err)
+				http.Error(w, "Server error", http.StatusInternalServerError)
+			}
+		})
+	}
+
+	addr := ":8080"
+	fmt.Println("Listenin' on", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		fmt.Println("Server error:", err)
+	}
+
+	// fmt.Println("asd asd cock");
+	// libgit_init()
+
+	// repo, err := repository_open(".")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)  // maybe copy the exit status of the libgit error?
+	// }
+
+	// names, err := list_all(repo)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Println(names)
 }
